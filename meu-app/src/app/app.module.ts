baa6768cf90e161050c4a46cf36661f882e0b635
eRodriguesSantana <eduardo.r.santana@hotmail.com>
2021-01-26 15:26:30 -0300

    Criação de componente via linha de comando - automático

--- meu-app/src/app/app.module.ts
+++ meu-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MeuComponenteComponent } from './meu-componente/meu-componente-component';
+import { MeuComponente2Component } from './meu-componente2/meu-componente2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuComponenteComponent
+    MeuComponenteComponent,
+    MeuComponente2Component
   ],
   imports: [
     BrowserModule
