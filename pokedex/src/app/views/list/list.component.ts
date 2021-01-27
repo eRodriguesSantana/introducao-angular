c6db029f7409bd971230a4d1fae6f142aaf9025d
eRodriguesSantana <eduardo.r.santana@hotmail.com>
2021-01-27 13:36:40 -0300

    Aplicando Event e Property Binding ao projeto

--- pokedex/src/app/views/list/list.component.ts
+++ pokedex/src/app/views/list/list.component.ts
@@ -7,16 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  selectedPkm = null;
   pokemonList = [
     { name: 'Bulbasaur', number: 1 },
     { name: 'Charmander', number: 4 },
     { name: 'Squirtle', number: 7 },
     { name: 'Pikachu', number: 25 }
-  ]
+  ];
+
+  get pkmSprite() {
+    const NUMBER = ('000' + this.selectedPkm.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${NUMBER}.png`;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectPokemon(pkm) {
+    this.selectedPkm = pkm;
   }
 
 }
